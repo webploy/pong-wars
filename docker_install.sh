8a47c065e4aced80ab3c4823b21940e1ece686d1
Fernando Tapia Rico <fernando@tapiarico.com>
2025-06-01 20:48:36 +0200

    Create docker_install.sh

---
+++ docker_install.sh
@@ -0,0 +1,15 @@
+
+if ! command -v docker >/dev/null 2>&1; then
+echo "Installing Docker..." 
+apt-get update
+apt-get install -y ca-certificates curl
+apt --reinstall install coreutils
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian $(. /etc/os-release && echo "$VERSION_CODENAME") stable" > /etc/apt/sources.list.d/docker.list
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+fi
+echo "Docker installed"
